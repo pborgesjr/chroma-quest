94a4e7487dbcb13330de4a9a4f9466f5910abfec
Pedro Borges <ppmbjr@gmail.com>
2023-03-08 02:48:42 -0300

    chore: added ConfigContext to App.tsx

--- App.tsx
+++ App.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaView} from 'react-native';
 
-import {GameProvider} from './src/context';
+import {ConfigProvider, GameProvider} from './src/context';
 import {StackNavigator} from './src/routes';
 
 function App(): JSX.Element {
   return (
     <NavigationContainer>
       <GameProvider>
-        <SafeAreaView style={{flex: 1}}>
-          <StackNavigator />
-        </SafeAreaView>
+        <ConfigProvider>
+          <SafeAreaView style={{flex: 1}}>
+            <StackNavigator />
+          </SafeAreaView>
+        </ConfigProvider>
       </GameProvider>
     </NavigationContainer>
   );
