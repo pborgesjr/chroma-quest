20803e0f6bf1559941e8ed6cbde4af88c2616d2f
Pedro Borges <ppmbjr@gmail.com>
2023-03-08 14:15:44 -0300

    feat: added color blind color palette

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -10,6 +10,7 @@ export const COLOR_PALETTE = {
     white: '#fff',
     whitec4: '#c4c4c4',
     black: '#000',
+    darkGrey: '#1D1C1C',
     blackTransparent: 'rgba(0, 0, 0, 0.88)',
   },
   normal: {
@@ -28,6 +29,60 @@ export const COLOR_PALETTE = {
     4: {
       primary: '#f6ff00',
       secondary: '#bdc400',
+    },
+  },
+  tritanopia: {
+    1: {
+      primary: '#ff0004',
+      secondary: '#C90000',
+    },
+    2: {
+      primary: '#00575b',
+      secondary: '#00ADB5',
+    },
+    3: {
+      primary: '#75ecff',
+      secondary: '#4A98A4',
+    },
+    4: {
+      primary: '#c8c7d7',
+      secondary: '#90909E',
+    },
+  },
+  protanopia: {
+    1: {
+      primary: '#8e7e21',
+      secondary: '#D2B928',
+    },
+    2: {
+      primary: '#004c9d',
+      secondary: '#007AFF',
+    },
+    3: {
+      primary: '#f8dc00',
+      secondary: '#8A7A00',
+    },
+    4: {
+      primary: '#abb2d9',
+      secondary: '#5A5D73',
+    },
+  },
+  deuteranopia: {
+    1: {
+      primary: '#7f5f00',
+      secondary: '#B58700',
+    },
+    2: {
+      primary: '#004977',
+      secondary: '#0073BD',
+    },
+    3: {
+      primary: '#ffd599',
+      secondary: '#AF9166',
+    },
+    4: {
+      primary: '#b6a2ac',
+      secondary: '#675B60',
     },
   },
   shadow: '#00000073',
